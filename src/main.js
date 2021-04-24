a32936d05972d5fcc117e0474bd0e234a91f304f
Tate <tpieper@ucsc.edu>
2021-04-24 15:06:50 -0700

    Updated spaceship assets

--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
+/*
+Tate Pieper - 4/23/2021
+Rocket Patrol - Modded
+12 hours
+
+Point Breakdown:
+ 10 Points
+ - Implement parallax scrolling
+ - Create a new title screen (e.g., new artwork, typography, layout)
+ - Display time remaining
+ - Replace UI border
+ 20 Points
+ - Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points
+ - Create new artwork for all of the in-game assets (rocket, spaceships, explosion)
+ - Implement a new timing/scoring mechanism that adds time to the clock for successful hits
+
+ Total points: 100
+*/
+
+<<<<<<< HEAD
+
+=======
+>>>>>>> 7bdb1216e1562263e6750d7f1919991abf303c33
 let config = {
     type: Phaser.AUTO,
     width: 640,
